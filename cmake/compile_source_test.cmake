fb89b3e77b5ed0dba30af41c0d58cb89bf924968
Brad Bell <bradbell@seanet.com>
2021-09-06 05:39:53 -0700

    actions: Trying to get mkstep detection to work on macos-latest.

--- cmake/compile_source_test.cmake
+++ cmake/compile_source_test.cmake
@@ -33,18 +33,16 @@ FUNCTION(compile_source_test source variable)
         )
     ENDIF( DEFINED ${variable} )
     #
-    IF( DEFINED compiles_source_test_result)
-        UNSET(compiles_source_test_result)
-    ENDIF( DEFINED compiles_source_test_result )
-    #
     # check that source codee compiles
     CHECK_CXX_SOURCE_COMPILES("${source}" compiles_source_test_result )
     #
     # change result varialbe to 0 (1) for fail (succeed).
     IF( compiles_source_test_result )
         SET(${variable} 1)
+        SET(${variable} 1 PARENT_SCOPE )
     ELSE( compiles_source_test_result )
         SET(${variable} 0)
+        SET(${variable} 0 PARENT_SCOPE)
     ENDIF( compiles_source_test_result )
     #
     # check that varialbe is defined
